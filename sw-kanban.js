0e4d8a166de3835086c0081251b4634262e55dd8
TorredeControlCSS <pedregaljuandiaz@gmail.com>
2026-09-20 18:28:57 -0500

    Update cache version to 'kanban-v64'

--- sw-kanban.js
+++ sw-kanban.js
@@ -12,7 +12,7 @@
  * anterior instalada en el celular.
  * =========================================================================== */
 
-const CACHE_VER = 'kanban-v63';
+const CACHE_VER = 'kanban-v64';
 
 // Solo el armazón. Nada de datos.
 const ASSETS = [
